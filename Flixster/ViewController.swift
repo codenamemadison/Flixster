eccadeaf107207bc525d59783e50f3dd294e0c37
codenamemadison <maddymad138@icloud.com>
2021-09-26 19:09:37 -0400

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Madison Shimbo on 9/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
